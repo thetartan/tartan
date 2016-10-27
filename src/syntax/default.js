c103cd90e8e2e2e14c8c38e93e52fa369af1a066
Levko Kravets <levko.ne@gmail.com>
2016-10-27 12:02:28 +0300

    Allow to parse different warp and weft sequences

--- src/syntax/default.js
+++ src/syntax/default.js
@@ -12,38 +12,53 @@ var defaultOptions = {
   failOnUnsupportedTokens: true,
   // function to filter parsed tokens: (tokens) => { return modifiedTokens; }
   filterTokens: null,
+  // Should return `true` if token is a separator between
+  // warp and weft sequence
+  isWarpAndWeftSeparator: function(token) { return false; },
+  failOnMultipleWarpAndWeftSeparators: true,
   // function to transform newly built AST: (sett) => { return modifiedSett; }
   transformSett: null
 };
 
-function buildSyntaxTree(tokens, options) {
-  // Some pre-validation and filtering
-  if (!_.isArray(tokens)) {
-    return tokens;
-  }
-  if (_.isFunction(options.filterTokens)) {
-    tokens = options.filterTokens(tokens);
-    if (!_.isArray(tokens)) {
-      return tokens;
+function splitWarpAndWeft(tokens, options) {
+  var result = {warp: [], weft: []};
+  var current = result.warp;
+  var isWeftStarted = false;
+  _.each(tokens, function(token) {
+    if (options.isWarpAndWeftSeparator(token)) {
+      if (isWeftStarted) {
+        if (options.failOnMultipleWarpAndWeftSeparators) {
+          throw new errors.InvalidToken(token);
+        }
+      } else {
+        current = result.weft;
+        isWeftStarted = true;
+      }
+    } else {
+      current.push(token);
     }
+  });
+
+  if (result.warp.length == 0) {
+    result.warp = result.weft;
+  }
+  if (result.weft.length == 0) {
+    result.weft = result.warp;
   }
 
-  // Internal filters
-  tokens = pivotsToSquareBrackets(options)(tokens);
-  tokens = matchSquareBrackets()(tokens);
+  return result;
+}
 
-  var result = {};
-  result.colors = {};
-  result.warp = result.weft = [];
-
-  var stack = [result.warp];
+function buildTree(tokens, colors, options) {
+  var result = [];
+  var stack = [result];
   var currentBlock;
   var temp;
 
   _.each(tokens, function(token) {
     switch (token.type) {
       case utils.TokenType.color:
-        result.colors[token.name] = token.color;
+        colors[token.name] = token.color;
         break;
       case utils.TokenType.stripe:
         currentBlock = _.last(stack);
@@ -75,6 +90,39 @@ function buildSyntaxTree(tokens, options) {
     }
   });
 
+  return result;
+}
+
+function buildSyntaxTree(tokens, options) {
+  // Some pre-validation and filtering
+  if (!_.isArray(tokens)) {
+    return tokens;
+  }
+  if (_.isFunction(options.filterTokens)) {
+    tokens = options.filterTokens(tokens);
+    if (!_.isArray(tokens)) {
+      return tokens;
+    }
+  }
+
+  tokens = splitWarpAndWeft(tokens, options);
+  var warpIsSameAsWeft = tokens.warp === tokens.weft;
+
+  var result = {};
+  result.colors = {};
+
+  // Internal filters
+  tokens.warp = pivotsToSquareBrackets(options)(tokens.warp);
+  tokens.warp = matchSquareBrackets()(tokens.warp);
+  result.warp = buildTree(tokens.warp, result.colors, options);
+  if (warpIsSameAsWeft) {
+    result.weft = result.warp;
+  } else {
+    tokens.weft = pivotsToSquareBrackets(options)(tokens.weft);
+    tokens.weft = matchSquareBrackets()(tokens.weft);
+    result.weft = buildTree(tokens.weft, result.colors, options);
+  }
+
   if (_.isFunction(options.transformSett)) {
     result = options.transformSett(result);
   }
@@ -84,6 +132,9 @@ function buildSyntaxTree(tokens, options) {
 
 function factory(options) {
   options = _.extend({}, defaultOptions, options);
+  if (!_.isFunction(options.isWarpAndWeftSeparator)) {
+    options.isWarpAndWeftSeparator = defaultOptions.isWarpAndWeftSeparator;
+  }
   return function(tokens) {
     return buildSyntaxTree(tokens, options);
   };
