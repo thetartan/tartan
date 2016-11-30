e36e87704c0bd9cb77759ff28ff8e921747e14fd
Levko Kravets <levko.ne@gmail.com>
2016-11-30 20:12:51 +0200

    Prepare for improvements of 'fold' transform

--- src/transform/fold.js
+++ src/transform/fold.js
@@ -1,54 +1,71 @@
 'use strict';
 
 var _ = require('lodash');
+var utils = require('../utils');
 
 var defaultOptions = {
 };
 
-function processTokens(root, options) {
+function foldRootBlock(root, options, results) {
   if (root.reflect || (root.items.length % 2 != 0)) {
-    return root;
+    return;
   }
   // Smallest reflective sett contains 3 stripes in threadcount or
   // 4 stripes when unfolded, i.e. R/10 K2 Y/2 => R10 K2 Y2 K2;
   // R/10 K/2 => R10 K2
   if (root.items.length < 4) {
-    return root;
+    return;
   }
 
-  var result = [];
+  var items = [];
   var i = 1;
   var j = root.items.length - 1;
   var left;
   var right;
 
-  result.push(root.items[0]);
+  items.push(root.items[0]);
   while (true) {
     left = root.items[i];
     right = root.items[j];
-    if (left.isStripe && right.isStripe) {
-      var isSameColor = left.name == right.name;
-      var isSameCount = left.count == right.count;
-      if (isSameColor && isSameCount) {
-        result.push(left);
-        if (i == j) {
-          break;
-        }
-        i++;
-        j--;
-        continue;
+    if (utils.node.isSameNode(left, right)) {
+      items.push(left);
+      if (i == j) {
+        break;
       }
+      i++;
+      j--;
+      continue;
     }
-    result = null;
-    break;
+    return;
   }
 
-  if (result) {
-    root = _.clone(root);
-    root.items = result;
+  if (items) {
+    var result = _.clone(root);
+    root.items = items;
     root.reflect = true;
+    results.push({
+      node: result,
+      hash: utils.node.calculateNodeHash(result),
+      weight: utils.node.calculateNodeWeight(result)
+    });
   }
-  return root;
+}
+
+function processTokens(root, options) {
+  var variants = [{
+    node: root,
+    hash: utils.node.calculateNodeHash(root),
+    weight: utils.node.calculateNodeWeight(root)
+  }];
+  foldRootBlock(root, options, variants);
+  return _.chain(variants)
+    .uniqBy(function(item) {
+      return item.hash;
+    })
+    .sortBy(function(item) {
+      return item.weight;
+    })
+    .value();
 }
 
 function transform(sett, options) {
@@ -56,15 +73,19 @@ function transform(sett, options) {
 
   var warpIsSameAsWeft = sett.warp === sett.weft;
   if (_.isObject(sett.warp)) {
-    result.warp = processTokens(sett.warp, options);
+    result.warpVariants = processTokens(sett.warp, options);
   }
   if (_.isObject(sett.weft)) {
     if (warpIsSameAsWeft) {
-      result.weft = result.warp;
+      result.weftVariants = result.warpVariants;
     } else {
-      result.weft = processTokens(sett.weft, options);
+      result.weftVariants = processTokens(sett.weft, options);
     }
   }
+
+  // Take the best variant, but keep other too
+  result.warp = _.first(result.warpVariants).node;
+  result.weft = _.first(result.weftVariants).node;
 
   return result;
 }
--- src/utils/node.js
+++ src/utils/node.js
@@ -26,6 +26,112 @@ function newRootBlock(items, reflect, repeat) {
   return result;
 }
 
+function parseRepeat(value) {
+  var result = parseInt(value, 10) || 0;
+  return result > 1 ? result : 1;
+}
+
+function calculateNodeHash(node) {
+  if (!_.isObject(node)) {
+    return '';
+  }
+  if (node.isStripe && (node.count > 0)) {
+    return node.name + node.count * parseRepeat(node.repeat);
+  }
+  if (node.isBlock) {
+    if (_.isArray(node.items) && (node.items.length > 0)) {
+      return '[' + (node.isRoot ? 'R' : 'B') +
+        '*' + parseRepeat(node.repeat) +
+        '/' + (node.reflect ? 'RF' : 'RP') + ':' +
+        _.chain(node.items)
+          .map(calculateNodeHash)
+          .join('')
+          .value()
+        + ']';
+    }
+  }
+  return '';
+}
+
+function calculateNodeWeight(node, returnRawWeight) {
+  var stripeCount = 0;
+  var blockCount = 0;
+
+  if (_.isObject(node)) {
+    if (node.isStripe) {
+      stripeCount++;
+    }
+
+    if (node.isBlock && _.isArray(node.items) && (node.items.length > 0)) {
+      blockCount++;
+      _.each(node.items, function(item) {
+        if (_.isObject(item) && item.isBlock) {
+          var nested = calculateNodeWeight(item, true);
+          blockCount += nested.blocks;
+        }
+      });
+    }
+  }
+
+  if (returnRawWeight) {
+    return {blocks: blockCount, stripes: stripeCount};
+  }
+
+  if ((blockCount == 0) && (stripeCount == 0)) {
+    return Number.MAX_VALUE;
+  }
+
+  return Math.sqrt(blockCount * blockCount + stripeCount * stripeCount);
+}
+
+function isSameNode(left, right) {
+  if (!_.isObject(left) || (!_.isObject(right))) {
+    return false;
+  }
+  if (left.isStripe && right.isStripe) {
+    return (left.name == right.name) && (
+      left.count * parseRepeat(left.repeat) ==
+      right.count * parseRepeat(right.repeat)
+    );
+  }
+  if (left.isBlock && right.isBlock) {
+    // Both should be root or not
+    if (left.isRoot != right.isRoot) {
+      return false;
+    }
+    // Both should be reflected or not
+    if (left.reflect != right.reflect) {
+      return false;
+    }
+    // Both should be repeated equal times
+    if (parseRepeat(left.repeat) != parseRepeat(right.repeat)) {
+      return false;
+    }
+    // Both should have same count of items
+    if (!_.isArray(left.items) || !_.isArray(right.items)) {
+      return false;
+    }
+    if (left.items.length != right.items.length) {
+      return false;
+    }
+
+    // Complex case: compare all items
+    var result = true;
+    _.each(left.items, function(leftItem, index) {
+      var rightItem = right.items[index];
+      if (!isSameNode(leftItem, rightItem)) {
+        result = false;
+        return false; // Break
+      }
+    });
+    return result;
+  }
+  return false;
+}
+
 module.exports.newStripe = newStripe;
 module.exports.newBlock = newBlock;
 module.exports.newRootBlock = newRootBlock;
+module.exports.isSameNode = isSameNode;
+module.exports.calculateNodeHash = calculateNodeHash;
+module.exports.calculateNodeWeight = calculateNodeWeight;
